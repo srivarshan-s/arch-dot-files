db02b535fc96526115f22cc9277d87ea9bdc44b2
Srivarshan Selvaraj <srivarshan02@gmail.com>
2022-12-31 13:19:23 +0530

    Add install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# INSTALL BASHRC
+ln -nfs ${BASEDIR}/.bashrc ~/.bashrc
+
+# INSTALL FONTS
+ln -nfs ${BASEDIR}/.fonts ~/.fonts
+
+# INSTALL GIT CONFIG
+ln -nfs ${BASEDIR}/.gitconfig ~/.gitconfig
+
+# INSTALL TMUX CONFIG
+ln -nfs ${BASEDIR}/.tmux.conf ~/.tmux.conf
+
+# INSTALL ALACRITTY
+ln -nfs ${BASEDIR}/alacritty ~/.config/alacritty
+
+# INSTALL FISH
+ln -nfs ${BASEDIR}/fish ~/.config/fish
