a8370184ca604e124a0cd17e566a7f3527a634db
Srivarshan Selvaraj <srivarshan02@gmail.com>
2023-01-03 11:00:24 +0530

    Add function to start docker dev container

--- .bashrc
+++ .bashrc
@@ -8,5 +8,12 @@ PS1='[\u@\h \W]\$ '
 # ALIASES
 alias ls='ls --color=auto'
 
+# DOCKER FUNCTIONS
+my-ubuntu-dev () {
+    docker run -it --name my-ubuntu -v $(pwd):/workspace ubuntu-dev-1
+    docker commit my-ubuntu ubuntu-dev-1
+    docker rm my-ubuntu
+}
+
 # START FISH SHELL
 exec fish
--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,13 @@
 # STARSHIP PROMPT
 starship init fish | source
 
+# DOCKER FUNCTIONS
+function my-ubuntu-dev
+    docker run -it --name my-ubuntu -v $(pwd):/workspace ubuntu-dev-1
+    docker commit my-ubuntu ubuntu-dev-1
+    docker rm my-ubuntu
+end
+
 # ALIASES
 alias paru='paru --skipreview'
 alias ls='exa'
